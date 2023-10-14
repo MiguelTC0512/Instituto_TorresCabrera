e24c4a7820a5b6697da21041745b3902b9a3d1dc
MiguelTC0512 <1360478@senati.pe>
2023-10-13 20:30:46 -0500

    primer commit

---
+++ Models/Bloque.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Instituto.Models
+{
+    public partial class Bloque
+    {
+        public Bloque()
+        {
+            Matriculas = new HashSet<Matricula>();
+        }
+
+        public int IdBloque { get; set; }
+        public string NombreBloque { get; set; } = null!;
+        public DateTime PeriodoBloque { get; set; }
+
+        public virtual ICollection<Matricula> Matriculas { get; set; }
+    }
+}
---
+++ Models/Matricula.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Instituto.Models
+{
+    public partial class Matricula
+    {
+        public int IdMatricula { get; set; }
+        public int IdEstudiante { get; set; }
+        public int IdBloque { get; set; }
+
+        public virtual Bloque IdBloqueNavigation { get; set; } = null!;
+        public virtual Estudiante IdEstudianteNavigation { get; set; } = null!;
+    }
+}
